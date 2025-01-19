0c5077291d7d065c5bd89bf4bf934e5ac1f2e56e
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-01-19 23:36:38 +0700

    Update py.py

--- py.py
+++ py.py
@@ -1,7 +1,7 @@
 
 # MIT License
 
-# Copyright 2024 @asyncze (Michael Sjöberg)
+# Copyright 2024, 2025 @asyncze (Michael Sjöberg)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
