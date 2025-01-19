e0513da4a5932f146922fb3917a2bd706306e6f0
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-01-19 10:24:11 +0700

    Update txt.py

--- txt.py
+++ txt.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Tokenizer for Text
+# (non?)-Tokenizer for Plain Text
 
 # import re
 from enum import Enum
