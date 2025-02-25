00c264d8071f5ac639623eefb5433c212ce72c06
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-02-26 00:10:23 +0700

    Update hackerman.py

--- hackerman.py
+++ hackerman.py
@@ -50,7 +50,7 @@ COMMENT = "comment"
 SPECIAL = "special"
 WARNING = "warning"
 
-lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "hackerman_odin.dylib"))
+lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "hackerman.dylib"))
 
 # Odin internal struct for string (rawptr and len)
 class String(ctypes.Structure):
