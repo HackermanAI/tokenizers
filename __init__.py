c7d93b2bfb54b3106a0d903556c5a9dda5794e73
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-03-13 08:39:00 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+
+# from .txt import CustomLexerText
+# from .hackerman import CustomLexerHackerman
+# from .odin import CustomLexerOdin
+# from .pc import CustomLexerPlayCode
+# from .todo import CustomLexerTodo
