45cb13159fe97f8dd0c4dbc7cc39de1c69ae1524
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-05-04 00:22:26 +0800

    Update todo.pyx

--- todo.pyx
+++ todo.pyx
@@ -51,7 +51,67 @@ cdef int handle_header(int current_char_index, str text, list tokens):
         current_char_index += 1
 
     tokens.append((KEYWORD, start_pos, line))
+    return current_char_index
 
+cdef int handle_done_task(int current_char_index, str text, list tokens):
+    cdef int start_pos = current_char_index
+    cdef str line = text[current_char_index]
+    current_char_index += 1
+
+    while current_char_index < len(text) and text[current_char_index] != '\n':
+        line += text[current_char_index]
+        current_char_index += 1
+
+    tokens.append((SUCCESS, start_pos, line))
+    return current_char_index
+
+cdef int handle_not_done_task(int current_char_index, str text, list tokens):
+    cdef int start_pos = current_char_index
+    cdef str line = text[current_char_index]
+    current_char_index += 1
+
+    cdef str special = ""
+
+    while current_char_index < len(text) and text[current_char_index] != '\n':
+        if text[current_char_index] == '[':
+            tokens.append((NAME, start_pos, line))
+            
+            # update state
+            start_pos = current_char_index
+            special = text[current_char_index]
+            current_char_index += 1
+
+            while current_char_index < len(text) and text[current_char_index] != '\n':
+                special += text[current_char_index]
+                if text[current_char_index] == ']':
+                    current_char_index += 1
+                    break
+                current_char_index += 1
+
+            tokens.append((SPECIAL, start_pos, special))
+
+            if current_char_index < len(text) and text[current_char_index] == ' ':
+                current_char_index += 1
+
+            start_pos = current_char_index
+            line = ""
+        else:
+            line += text[current_char_index]
+            current_char_index += 1
+
+    tokens.append((NAME, start_pos, line))
+    return current_char_index
+
+cdef int handle_priority_task(int current_char_index, str text, list tokens):
+    cdef int start_pos = current_char_index
+    cdef str line = text[current_char_index]
+    current_char_index += 1
+
+    while current_char_index < len(text) and text[current_char_index] != '\n':
+        line += text[current_char_index]
+        current_char_index += 1
+
+    tokens.append((ERROR, start_pos, line))
     return current_char_index
 
 @cython.cclass
@@ -64,9 +124,6 @@ class Lexer:
 
     def tokenize(self, str text):
         cdef int current_char_index = 0
-        cdef int start_pos
-        cdef str line
-        cdef str special
         cdef str current_char
         cdef list tokens = []
 
@@ -77,64 +134,12 @@ class Lexer:
             if current_char in (' ', '\t', '\r', '\n'): current_char_index = handle_whitespace(current_char_index)
             # header
             elif current_char == '#': current_char_index = handle_header(current_char_index, text, tokens)
-
-            # done
-            elif current_char == '+':
-                start_pos = current_char_index
-                line = current_char
-                current_char_index += 1
-
-                while current_char_index < len(text) and text[current_char_index] != '\n':
-                    line += text[current_char_index]
-                    current_char_index += 1
-
-                tokens.append((SUCCESS, start_pos, line))
-
-            # not done
-            elif current_char == '-':
-                start_pos = current_char_index
-                line = current_char
-                current_char_index += 1
-
-                while current_char_index < len(text) and text[current_char_index] != '\n':
-                    if text[current_char_index] == '[':
-                        tokens.append((NAME, start_pos, line))
-                        start_pos = current_char_index
-                        special = text[current_char_index]
-                        current_char_index += 1
-
-                        while current_char_index < len(text) and text[current_char_index] != '\n':
-                            special += text[current_char_index]
-                            if text[current_char_index] == ']':
-                                current_char_index += 1
-                                break
-                            current_char_index += 1
-
-                        tokens.append((SPECIAL, start_pos, special))
-
-                        if current_char_index < len(text) and text[current_char_index] == ' ':
-                            current_char_index += 1
-
-                        start_pos = current_char_index
-                        line = ""
-                    else:
-                        line += text[current_char_index]
-                        current_char_index += 1
-
-                tokens.append((NAME, start_pos, line))
-
-            # important
-            elif current_char == '*':
-                start_pos = current_char_index
-                line = current_char
-                current_char_index += 1
-
-                while current_char_index < len(text) and text[current_char_index] != '\n':
-                    line += text[current_char_index]
-                    current_char_index += 1
-
-                tokens.append((ERROR, start_pos, line))
-
+            # done task
+            elif current_char == '+': current_char_index = handle_done_task(current_char_index, text, tokens)
+            # not done task
+            elif current_char == '-': current_char_index = handle_not_done_task(current_char_index, text, tokens)
+            # priority task
+            elif current_char == '*': current_char_index = handle_priority_task(current_char_index, text, tokens)
             # style everything else as comment
             else:
                 tokens.append((COMMENT, current_char_index, current_char))
