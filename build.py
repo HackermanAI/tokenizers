31188620e97a648a0a5f1b2bea5e2b872a44f8c4
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-05-04 01:16:16 +0800

    Update build.py

--- build.py
+++ build.py
@@ -12,3 +12,4 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 setup(ext_modules=cythonize([Extension("todo", ["todo.pyx"])], compiler_directives={ "language_level": "3" }))
+setup(ext_modules=cythonize([Extension("hackerman", ["hackerman.pyx"])], compiler_directives={ "language_level": "3" }))
