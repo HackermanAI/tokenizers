f6e49344d4520f70ce2b25fecdba4867b9cdf3e9
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-05-03 19:21:34 +0800

    Create build.py

---
+++ build.py
@@ -0,0 +1,14 @@
+
+# Build file for Cython(.pyx) files
+
+# Cython     3.0.12
+# pip        24.2
+# setuptools 80.3.0
+
+# export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+# .venv/bin/python build.py build_ext --inplace
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+setup(ext_modules=cythonize([Extension("todo", ["todo.pyx"])], compiler_directives={ "language_level": "3" }))
