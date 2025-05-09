88a5ebd59ffcbd57ccd08f2575b7f07f480392b3
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-05-09 19:10:07 +0800

    Update scrpd.py

--- scrpd.py
+++ scrpd.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Tokenizer for Scratch Pad (.pyx-version)
+# Tokenizer for Scratch Pad (.pyx)
 
 # cython: language_level=3
 cimport cython
