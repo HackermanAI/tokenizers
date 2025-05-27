912a94e2df1ec3ffedf66752a9c1110d27580547
Michael <mail@michaelsjoeberg.com>
2025-05-27 10:43:33 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ odin build odin.odin -file -build-mode:dll
 odin build hackerman.odin -file -build-mode:dll
 odin build pc.odin -file -build-mode:dll
 
+export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
 .venv/bin/python build.py build_ext --inplace
