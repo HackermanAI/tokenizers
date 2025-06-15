a89ab8e37078511d162709b395b2a3f23c642341
Michael <mail@michaelsjoeberg.com>
2025-06-15 14:36:21 +0700

    moved old stuff

--- __init__.py
+++
@@ -1,6 +0,0 @@
-
-# from .txt import CustomLexerText
-# from .hackerman import CustomLexerHackerman
-# from .odin import CustomLexerOdin
-# from .pc import CustomLexerPlayCode
-# from .todo import CustomLexerTodo
