001607f21f99fb65e691d3be3073f83fa6f33c37
Michael <mail@michaelsjoeberg.com>
2025-06-15 13:38:39 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-odin build odin.odin -file -build-mode:dll
-odin build hackerman.odin -file -build-mode:dll
+# odin build odin.odin -file -build-mode:dll
+# odin build hackerman.odin -file -build-mode:dll
 odin build pc.odin -file -build-mode:dll
 
 export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
