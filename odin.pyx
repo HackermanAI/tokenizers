11de23fc5f3ac4b3dce6d58e3a3c3dfa3576fe2a
Michael <mail@michaelsjoeberg.com>
2025-08-10 20:58:28 +0700

    Update odin.pyx

--- odin.pyx
+++ odin.pyx
@@ -26,6 +26,7 @@
 # cython: language_level=3
 cimport cython
 
+# --- Token Types ---
 cdef str WHITESPACE = "whitespace"
 cdef str DEFAULT = "default"
 cdef str KEYWORD = "keyword"
@@ -46,6 +47,7 @@ cdef str ERROR = "error"
 cdef str WARNING = "warning"
 cdef str SUCCESS = "success"
 
+# --- Keyword and Built-in Sets ---
 KEYWORDS = frozenset({ 
     "asm",
     "auto_cast",
@@ -114,117 +116,125 @@ TYPES = frozenset({
     "bool",
 })
 
-cdef int handle_attribute(int current_char_index, str text, list tokens):
-    cdef int start_pos = current_char_index
-    cdef str line = text[current_char_index]
-    current_char_index += 1
+# --- Helper Functions for Tokenization ---
 
-    while current_char_index < len(text) and (text[current_char_index].isalnum() or text[current_char_index] in { '_', '(', ')' }):
-        line += text[current_char_index]
+cdef int handle_attribute(int current_char_index, str text, int length, list tokens):
+    cdef int start_pos = current_char_index
+    # cdef str line = text[current_char_index]
+    current_char_index += 1 # Consume '@'
+
+    while current_char_index < length and (text[current_char_index].isalnum() or text[current_char_index] in { '_', '(', ')' }):
+        # line += text[current_char_index]
         current_char_index += 1
 
-    tokens.append((OPERATOR, start_pos, line))
+    tokens.append((OPERATOR, start_pos, text[start_pos:current_char_index]))
     return current_char_index
 
-cdef int handle_directive(int current_char_index, str text, list tokens):
+cdef int handle_directive(int current_char_index, str text, int length, list tokens):
     cdef int start_pos = current_char_index
-    cdef str line = text[current_char_index]
-    current_char_index += 1
+    # cdef str line = text[current_char_index]
+    current_char_index += 1 # Consume '#'
 
-    while current_char_index < len(text) and (text[current_char_index].isalnum() or text[current_char_index] == '_'):
-        line += text[current_char_index]
+    while current_char_index < length and (text[current_char_index].isalnum() or text[current_char_index] == '_'):
+        # line += text[current_char_index]
         current_char_index += 1
 
-    tokens.append((KEYWORD, start_pos, line))
+    tokens.append((KEYWORD, start_pos, text[start_pos:current_char_index]))
     return current_char_index
 
 cdef int handle_operator(int current_char_index, str text, list tokens):
     cdef int start_pos = current_char_index
-    cdef str line = text[current_char_index]
+    # cdef str line = text[current_char_index]
 
-    tokens.append((OPERATOR, start_pos, line))
+    tokens.append((OPERATOR, start_pos, text[current_char_index]))
     return current_char_index + 1
 
-cdef int handle_comment(int current_char_index, str text, list tokens):
+cdef int handle_comment(int current_char_index, str text, int length, list tokens):
     cdef int start_pos = current_char_index
-    cdef str line = text[current_char_index]
-    current_char_index += 1
+    # cdef str line = text[current_char_index]
+    current_char_index += 1 # Consume '//'
 
-    while current_char_index < len(text) and text[current_char_index] != '\n':
-        line += text[current_char_index]
+    while current_char_index < length and text[current_char_index] != '\n':
+        # line += text[current_char_index]
         current_char_index += 1
 
-    tokens.append((COMMENT, start_pos, line))
+    tokens.append((COMMENT, start_pos, text[start_pos:current_char_index]))
     return current_char_index
 
-cdef int handle_multiline_comment(int current_char_index, str text, list tokens):
+cdef int handle_multiline_comment(int current_char_index, str text, int length, list tokens):
     cdef int start_pos = current_char_index
-    cdef str line = text[current_char_index]
-    current_char_index += 1
+    # cdef str line = text[current_char_index]
+    current_char_index += 1 # Consume '/*'
 
-    while current_char_index + 1 < len(text):
+    while current_char_index + 1 < length:
         if text[current_char_index] == '*' and text[current_char_index + 1] == '/':
-            line += text[current_char_index]
-            line += text[current_char_index + 1]
+            # line += text[current_char_index]
+            # line += text[current_char_index + 1]
             current_char_index += 2
             
-            tokens.append((COMMENT, start_pos, line))
+            tokens.append((COMMENT, start_pos, text[start_pos:current_char_index]))
             return current_char_index
         
-        line += text[current_char_index]
+        # line += text[current_char_index]
         current_char_index += 1
 
-    tokens.append((ERROR, start_pos, line))
+    tokens.append((ERROR, start_pos, text[start_pos:current_char_index]))
     return current_char_index
 
-cdef int handle_string(int current_char_index, str text, list tokens):
+cdef int handle_string(int current_char_index, str text, int length, list tokens):
     cdef int start_pos = current_char_index
     cdef str quote = text[current_char_index]
-    cdef str line = quote
+    # cdef str line = quote
     current_char_index += 1
 
-    while current_char_index < len(text) and text[current_char_index] != quote:
-        line += text[current_char_index]
-        current_char_index += 1
+    while current_char_index < length and text[current_char_index] != quote:
+        # Handle escaped quotes
+        if text[current_char_index] == '\\' and current_char_index + 1 < length:
+            current_char_index += 2
+        else:
+            current_char_index += 1
 
-    if current_char_index < len(text) and text[current_char_index] == quote:
-        line += text[current_char_index]
+    if current_char_index < length and text[current_char_index] == quote:
+        # line += text[current_char_index]
         current_char_index += 1
-        tokens.append((STRING, start_pos, line))
+        tokens.append((STRING, start_pos, text[start_pos:current_char_index]))
     else:
-        tokens.append((ERROR, start_pos, line))
+        # Unterminated string
+        tokens.append((ERROR, start_pos, text[start_pos:current_char_index]))
     
     return current_char_index
 
-cdef int handle_number(int current_char_index, str text, list tokens):
+cdef int handle_number(int current_char_index, str text, int length, list tokens):
     cdef int start_pos = current_char_index
-    cdef str line = ""
+    # cdef str line = ""
 
-    while current_char_index < len(text) and (text[current_char_index].isdigit() or text[current_char_index] == '.'):
-        line += text[current_char_index]
+    while current_char_index < length and (text[current_char_index].isdigit() or text[current_char_index] == '.'):
+        # line += text[current_char_index]
         current_char_index += 1
 
-    tokens.append((NUMBER, start_pos, line))
+    tokens.append((NUMBER, start_pos, text[start_pos:current_char_index]))
     return current_char_index
 
-cdef int handle_identifier(int current_char_index, str text, list tokens):
+cdef int handle_identifier(int current_char_index, str text, int length, list tokens):
     cdef int start_pos = current_char_index
-    cdef str line = ""
+    # cdef str line = ""
 
-    while current_char_index < len(text) and (text[current_char_index].isalnum() or text[current_char_index] == '_'):
-        line += text[current_char_index]
+    while current_char_index < length and (text[current_char_index].isalnum() or text[current_char_index] == '_'):
+        # line += text[current_char_index]
         current_char_index += 1
 
-    if line in KEYWORDS:
-        tokens.append((KEYWORD, start_pos, line))
-    elif line in BUILT_INS:
-        tokens.append((BUILT_IN, start_pos, line))
-    elif line in TYPES:
-        tokens.append((TYPE, start_pos, line))
-    elif line in { "true", "false" }:
-        tokens.append((CONDITIONAL, start_pos, line))
+    cdef str identifier = text[start_pos:current_char_index]
+
+    if identifier in KEYWORDS:
+        tokens.append((KEYWORD, start_pos, identifier))
+    elif identifier in BUILT_INS:
+        tokens.append((BUILT_IN, start_pos, identifier))
+    elif identifier in TYPES:
+        tokens.append((TYPE, start_pos, identifier))
+    elif identifier in { "true", "false" }:
+        tokens.append((CONDITIONAL, start_pos, identifier))
     else:
-        tokens.append((DEFAULT, start_pos, line))
+        tokens.append((DEFAULT, start_pos, identifier))
 
     return current_char_index
 
@@ -232,36 +242,42 @@ cdef int handle_identifier(int current_char_index, str text, list tokens):
 class Lexer:
 
     @property
-    def lexer_name(self): return "Odin"
+    def lexer_name(self):
+        return "Odin"
 
     @property
-    def comment_char(self): return "//"
+    def comment_char(self):
+        return "//"
 
     def tokenize(self, str text):
         cdef int current_char_index = 0
+        cdef int length = len(text)
+        cdef list tokens = []
         cdef str current_char
         cdef str next_char
-        cdef list tokens = []
 
-        while current_char_index < len(text):
+        while current_char_index < length:
             current_char = text[current_char_index]
-            next_char = text[current_char_index + 1] if current_char_index + 1 < len(text) else ""
 
             # whitespace
             if current_char in { ' ', '\t', '\r', '\n' }:
                 current_char_index += 1
+                continue
+
+            next_char = text[current_char_index + 1] if current_char_index + 1 < length else ""
+
             # attribute
-            elif current_char == '@':
-                current_char_index = handle_attribute(current_char_index, text, tokens)
+            if current_char == '@':
+                current_char_index = handle_attribute(current_char_index, text, length, tokens)
             # directive
             elif current_char == '#':
-                current_char_index = handle_directive(current_char_index, text, tokens)
+                current_char_index = handle_directive(current_char_index, text, length, tokens)
             # comment
             elif current_char == '/' and next_char == '/':
-                current_char_index = handle_comment(current_char_index, text, tokens)
+                current_char_index = handle_comment(current_char_index, text, length, tokens)
             # multiline comment
             elif current_char == '/' and next_char == '*':
-                current_char_index = handle_multiline_comment(current_char_index, text, tokens)
+                current_char_index = handle_multiline_comment(current_char_index, text, length, tokens)
             # scope
             elif current_char == ':' and next_char == ':':
                 tokens.append((KEYWORD, current_char_index, current_char + next_char))
@@ -275,13 +291,13 @@ class Lexer:
                 current_char_index = handle_operator(current_char_index, text, tokens)            
             # string
             elif current_char in { '\"', '\'', '`' }:
-                current_char_index = handle_string(current_char_index, text, tokens)
+                current_char_index = handle_string(current_char_index, text, length, tokens)
             # number
             elif current_char.isdigit():
-                current_char_index = handle_number(current_char_index, text, tokens)
+                current_char_index = handle_number(current_char_index, text, length, tokens)
             # identifier
             elif current_char.isalpha() or current_char == '_':
-                current_char_index = handle_identifier(current_char_index, text, tokens)
+                current_char_index = handle_identifier(current_char_index, text, length, tokens)
             # default
             else:
                 tokens.append((DEFAULT, current_char_index, current_char))
