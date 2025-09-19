9dde420c0151b362303b95108827eaf241cab34a
Michael <mail@michaelsjoeberg.com>
2025-09-19 22:14:17 +0700

    Update build.py

--- build.py
+++ build.py
@@ -9,9 +9,9 @@ setup(
         [
             Extension("txt", ["txt.pyx"], extra_compile_args=["-O3", "-std=c11"]),
             Extension("hackerman", ["hackerman.pyx"], extra_compile_args=["-O3", "-std=c11"]),
-            Extension("scrpd", ["scrpd.pyx"], extra_compile_args=["-O3", "-std=c11"]),
+            # Extension("scrpd", ["scrpd.pyx"], extra_compile_args=["-O3", "-std=c11"]),
             Extension("todo", ["todo.pyx"], extra_compile_args=["-O3", "-std=c11"]),
-            Extension("odin", ["odin.pyx"], extra_compile_args=["-O3", "-std=c11"]),
+            # Extension("odin", ["odin.pyx"], extra_compile_args=["-O3", "-std=c11"]),
         ], 
         compiler_directives={ "language_level": "3" },
     )
