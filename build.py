c30b155aebd0f8de9064a3416db7a035d93f741f
Michael Sjöberg <mail@michaelsjoeberg.com>
2026-02-07 09:15:02 +0700

    Update build.py

--- build.py
+++ build.py
@@ -7,9 +7,9 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(
         [
-            Extension("txt", ["txt.pyx"], extra_compile_args=["-O3", "-std=c11"]),
+            # Extension("txt", ["txt.pyx"], extra_compile_args=["-O3", "-std=c11"]), # this is too slow on large text files to use as default for .txt
             Extension("hackerman", ["hackerman.pyx"], extra_compile_args=["-O3", "-std=c11"]),
-            Extension("scrpd", ["scrpd.pyx"], extra_compile_args=["-O3", "-std=c11"]),
+            # Extension("scrpd", ["scrpd.pyx"], extra_compile_args=["-O3", "-std=c11"]),
             Extension("pc", ["pc.pyx"], extra_compile_args=["-O3", "-std=c11"]),
         ], 
         compiler_directives={ "language_level": "3" },
