d2ba53d6a39ded54f2fadf601bad10512b896abd
Michael Sjöberg <mail@michaelsjoeberg.com>
2026-02-22 11:38:57 +0700

    Update build.py

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ setup(
     ext_modules=cythonize(
         [
             # Extension("txt", ["txt.pyx"], extra_compile_args=["-O3", "-std=c11"]), # this is too slow on large text files to use as default for .txt
-            Extension("stxt", ["stxt.pyx"], extra_compile_args=["-O3", "-std=c11"]),
+            # Extension("stxt", ["stxt.pyx"], extra_compile_args=["-O3", "-std=c11"]),
             Extension("hackerman", ["hackerman.pyx"], extra_compile_args=["-O3", "-std=c11"]),
             # Extension("scrpd", ["scrpd.pyx"], extra_compile_args=["-O3", "-std=c11"]),
             Extension("pc", ["pc.pyx"], extra_compile_args=["-O3", "-std=c11"]),
